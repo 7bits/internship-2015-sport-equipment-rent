57790cf9961db42046ae763340f9dd13e3354399
Awe66 <dart.fk712@gmail.com>
2015-07-09 12:52:08 +0600

    debuged data base

--- data_base.sql
+++ data_base.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Goods(
 	id bigserial not null,
 	title varchar(255) not null,
-	discription varchar(2048),
+	description varchar(2048),
 	pledge varchar(2048) not null,
 	price_per_hour double precision not null,
 	price_per_day double precision not null,
