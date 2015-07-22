7c6e5878e36843f5741e25835c1e160defa76f9a
Awe66 <dart.fk712@gmail.com>
2015-07-22 13:59:11 +0600

    add isAuthor boolean variable to see_announcement (Task 10895)

--- sport-rent-equipment/src/main/java/it/sevenbits/web/domain/Goods.java
+++ sport-rent-equipment/src/main/java/it/sevenbits/web/domain/Goods.java
@@ -17,6 +17,16 @@ public class Goods implements Serializable {
     Long authorId;
     String author;//name
     String authorPhone;
+    boolean visible;
+
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 
     public String getTitle() {
         return title;
