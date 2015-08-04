6f0fec9bc1ef2585f6bb89472a700f20a9c34db8
Awe66 <dart.fk712@gmail.com>
2015-08-04 13:43:34 +0600

    add spring handler for users profile

---
+++ sport-rent-equipment/src/main/java/it/sevenbits/web/controllers/user/ProfileOfOtherUsers.java
@@ -0,0 +1,44 @@
+package it.sevenbits.web.controllers.user;
+
+import it.sevenbits.web.domain.User;
+import it.sevenbits.web.service.users.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+/**
+ * Created by awemath on 8/4/15.
+ */
+@Controller
+@RequestMapping(value = "/profile")
+public class ProfileOfOtherUsers {
+    @Autowired
+    private UserService userService;
+
+    Logger LOG = Logger.getLogger(ProfileOfOtherUsers.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String login(@RequestParam(value="user_id", required = false) String userId, final Model model) {
+        User user = null;
+        try {
+            user = userService.getUser(Long.valueOf(userId));
+        } catch (Exception e) {
+            LOG.error("An error at class ProfileOfOtherUsers: "+e.getMessage());
+        }
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if(name.equals(user.getEmail())){
+            return "redirect:/personal_area";
+        }
+        model.addAttribute("isAuth", name!="anonymousUser");
+        model.addAttribute("user", user);
+        return "home/other_users_profile";
+    }
+}
