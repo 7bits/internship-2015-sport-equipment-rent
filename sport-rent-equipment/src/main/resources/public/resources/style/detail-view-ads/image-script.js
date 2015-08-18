468a58d589557ed7bce50474fe8d0884d513aff2
RotaninaJulia <rotanina.julia@gmail.com>
2015-08-18 13:44:59 +0600

    fix image in see announcement

---
+++ sport-rent-equipment/src/main/resources/public/resources/style/detail-view-ads/image-script.js
@@ -0,0 +1,28 @@
+(function($){
+    $('.one-foto-box img').each(
+        function(){
+            var $this=$(this);
+            var h=$this.height();
+            var w=$this.width();
+            var nh=100;   //высота до которой нужно масштабировать
+            var nw=131;  //ну и ширина
+            var k1=nh/nw;
+            var k2=h/w;
+
+            if (k1>k2)
+            {
+                h=h*(nw/w);
+                w=nw;
+            } else {
+                w=w*(nh/h);
+                h=nh;
+            }
+            $this.width(w);
+            $this.height(h);
+            if (h<nh) //Если картинка была слишком широкой, то при пропорциональном сжатии теряем в высоте, знач нужно выровнять по высоте
+            {
+                $this.css('margin-top',(nh-h)/2);
+            }
+        }
+    );
+})(jQuery);
