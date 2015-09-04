5d84dc6134c04a3a19ffd9fabb3b54b529ffcd5e
RotaninaJulia <rotanina.julia@gmail.com>
2015-09-04 11:01:58 +0600

    move scripts in add_announcement.jade

--- sport-rent-equipment/src/main/resources/public/resources/pages/add_announcement/add-photo.js
+++ sport-rent-equipment/src/main/resources/public/resources/pages/add_announcement/add-photo.js
@@ -40,16 +40,5 @@
 
 	});
 
-
-$(document).ready(function(){
-	$(".b-set-fields .b-photo").each(function() {
-		var fileVal = $(this).children('.b-download-mask_real-file-input').find('input').val();
-		if (fileVal==undefined){
-			$(this).children(".b-download-mask").find('.b-delete-photo_input-delete').hide();
-			$(this).children(".b-download-mask").find('.b-delete-photo').hide();
-		}
-	})
-	})
-
 })(jQuery);
 
