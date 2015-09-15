0108d9ecfb4307de43b04a67dbd7e4eaae8d45d2
RotaninaJulia <rotanina.julia@gmail.com>
2015-09-15 17:16:43 +0600

    fix blocks name

--- src/main/resources/public/resources/scripts/add-update-confirm-announcement/add-announcement-hide-delete-ico.js
+++ src/main/resources/public/resources/scripts/add-update-confirm-announcement/add-announcement-hide-delete-ico.js
@@ -1,9 +1,9 @@
 (function($) {
 $(document).ready(function(){
 	$(".b-set-fields .b-photo").each(function() {
-		var fileVal = $(this).children('.b-download-mask_real-file-input').find('input').val();
+		var fileVal = $(this).children('.b-download-mask__real-file-input').find('input').val();
 		if (fileVal==undefined){
-			$(this).children(".b-download-mask").find('.b-delete-photo_input-delete').hide();
+			$(this).children(".b-download-mask").find('.b-delete-photo__input-delete').hide();
 			$(this).children(".b-download-mask").find('.b-delete-photo').hide();
 		}
 	})
