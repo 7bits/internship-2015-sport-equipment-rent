c0ecaee559c8a9bf3aacde2dfd330933e5e030f0
RotaninaJulia <rotanina.julia@gmail.com>
2015-09-16 13:28:58 +0600

    header

--- src/main/resources/public/resources/scripts/header.js
+++ src/main/resources/public/resources/scripts/header.js
@@ -2,7 +2,12 @@
 $(document).ready(function(){
     $("ul").hide();
     $(this).find("h1 img").click(function(){
-        $(this).parent().next().slideToggle();
+        $(this).closest('.b-header').next('.b-header__dropdown-menu').show();
+        $(this).closest('.b-header').next('.b-header__dropdown-menu').find('ul').slideToggle();
+    });
+    $(this).find("h1 img").mouseenter(function(){
+        $(this).closest('.b-header').next('.b-header__dropdown-menu').show();
+        $(this).closest('.b-header').next('.b-header__dropdown-menu').find('ul').slideToggle();
     });
 });
 })(jQuery);
