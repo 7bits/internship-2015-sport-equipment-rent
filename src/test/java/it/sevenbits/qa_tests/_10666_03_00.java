97c99a99caefeb3a03d198fa724683dacae1f2bd
Marina Ovsyannikova <meliannaelf@gmail.com>
2015-09-21 16:46:27 +0600

    test (US-10666) implemented test cases for requirement 01 (_01), 03(_00)

---
+++ src/test/java/it/sevenbits/qa_tests/_10666_03_00.java
@@ -0,0 +1,88 @@
+package it.sevenbits.qa_tests;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+// Проверка на ввод пустого пароля
+public class _10666_03_00 extends TestCase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "/home/marina/chromedriver");
+    driver = new ChromeDriver();
+    baseUrl = "http://localhost:9000/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test106660300() throws Exception {
+    driver.get(baseUrl + "registration");
+    driver.findElement(By.id("name")).clear();
+    driver.findElement(By.id("name")).sendKeys("test");
+    driver.findElement(By.id("userEmail")).clear();
+    driver.findElement(By.id("userEmail")).sendKeys("me@test.com");
+    driver.findElement(By.cssSelector("input.b-button--default.b-button--blue")).click();
+    try {
+      assertEquals("Поле пароль не может быть пустым", driver.findElement(By.cssSelector("p")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+      assertEquals("Авторизация", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
