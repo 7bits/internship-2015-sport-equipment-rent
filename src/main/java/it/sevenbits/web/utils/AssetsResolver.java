0f8a6f1d8ec8e52e538ac6b2d042dafb374f4c24
RotaninaJulia <rotanina.julia@gmail.com>
2015-09-21 14:43:40 +0600

    not end finish gulp

---
+++ src/main/java/it/sevenbits/web/utils/AssetsResolver.java
@@ -0,0 +1,22 @@
+package it.sevenbits.web.utils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConfigurationProperties(prefix = "assets")
+public class AssetsResolver {
+        private String version;
+
+                public String getVersionedResourceUrl(final String filePath) {
+                final String filePathVersioned = filePath.replace("#version", version);
+                return filePathVersioned;
+            }
+
+                public String getVersion() {
+                return version;
+            }
+
+                public void setVersion(String version) {
+                this.version = version;
+            }
+    }
