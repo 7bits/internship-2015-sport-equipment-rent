94addec43a0bebcc6945e50050dba24481cfc515
RotaninaJulia <rotanina.julia@gmail.com>
2015-09-21 13:47:03 +0600

    add gulp not end

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp  = require('gulp'),
+    postcss      = require('gulp-postcss'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    minifyCss = require('gulp-minify-css'),
+    concatCss = require('gulp-concat-css'),
+    rename = require("gulp-rename");
+    yaml = require('js-yaml');
+    fs = require('fs');
+
+
+gulp.task('prod', [
+    'css:concat',
+    'css:autoprefixer',
+    'css:minify'
+    'css:hash'
+]);
+
+gulp.task('dev', [
+
+]);
+
+gulp.task('css:concat', function () {
+  return gulp.src('src/main/resources/public/resources/*/*.css')
+    .pipe(concatCss("result.css"))
+    .pipe(rename('result.min.css'))
+    .pipe(gulp.dest('src/main/resources/public/resources/build/'));
+});
+
+gulp.task('css:autoprefixer', function () {
+    return gulp.src('src/main/resources/public/resources/build/result.min.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/main/resources/public/resources/build/'));
+});
+
+gulp.task('css:minify', function() {
+  return gulp.src('src/main/resources/public/resources/build/result.min.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('src/main/resources/public/resources/build/'));
+});
+
+function readAssetsVersion() {
+  var version = '';
+
+  try {
+    var doc = yaml.safeLoad(fs.readFileSync('target/classes/config/application-prod.yml'));
+    version = doc.assets.version;
+  } catch (e) {
+    console.error('Fatal error. Before running scripts packaging package spring application');
+    version = '';
+  }
+
+  return version;
+}
+
+
+gulp.task('css:hash', function() {
+  var version = readAssetsVersion();
+  return gulp
+    .src('src/main/resources/public/resources/build/result.min.css')
+    .pipe(concatCss('bundle'+version+'.css'))
+    .pipe(gulp.dest('src/main/resources/public/resources/build/'));
+});
+
+
+//хэширование скриптов (пути чужие)
+gulp.task('scripts', function() {
+  var version = readAssetsVersion();
+  return gulp
+    .src('./src/main/resources/public/resources/scripts/*.js')
+    .pipe(concat('bundle'+version+'.js'))
+    .pipe(gulp.dest('./src/main/resources/public/dist/scripts/'));
+});
+
+//gulp.task('default', ['scripts', 'css:hash']);
