ad8694cf1eb918920b4ab5fb4d5f0936163dfdbf
RotaninaJulia <rotanina.julia@gmail.com>
2015-09-30 18:31:55 +0600

    add not success result to form by ajax

--- src/main/resources/public/resources/scripts/submit.js
+++ src/main/resources/public/resources/scripts/submit.js
@@ -5,14 +5,24 @@
                 form: $('.js-from').val(),
                 to: $('.js-to').val()
             }
+            /*isSuccess=true https://api.myjson.com/bins/2mree
+            isSucces=false  https://api.myjson.com/bins/4d4rq*/
             $.ajax({
-                url: "https://api.myjson.com/bins/4vbv2",
+                url: "https://api.myjson.com/bins/4d4rq",
                 dataType: 'json',
                 type: 'GET', /*POST*/
                 data: sendInfo,
                 /*headers: {'X-CSRF-TOKEN': $('meta[name = _csrf]').attr('content') }, */
                 success: function(data, textStatus, jqXHR) {
-                    $('.js-status-bar').show("slow");            
+                    if (data.isSuccess)
+                        $('.js-status-bar').show();
+                    else
+                        for (var i in data.errors){
+                            var buf = data.errors[i];
+                                $('input[name='+buf.key+']').addClass("js-input--with-error");
+                                $('.js-error').text(buf.value);
+                        }
+                    $('.js-errors').show();
                 }
             }).fail(function($xhr) {
                 var data = $xhr.responseJSON;
