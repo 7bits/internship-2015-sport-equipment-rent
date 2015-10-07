256429857055b2555d7121e7fc2d96899bb8e395
Awe66 <dart.fk712@gmail.com>
2015-10-07 18:04:30 +0600

    leave only String values at controller`s input

--- src/main/java/it/sevenbits/service/exceptions/AlreadyAnsweredException.java
+++ src/main/java/it/sevenbits/service/exceptions/AlreadyAnsweredException.java
@@ -3,6 +3,6 @@ package it.sevenbits.service.exceptions;
 /**
  * Created by awemath on 10/5/15.
  */
-public class AllreadyAnsweredException extends Exception {
+public class AlreadyAnsweredException extends Exception {
 
 }
