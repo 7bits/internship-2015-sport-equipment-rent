14214d6a37d6d29ef6795e6eb2b63dba56feecc1
Awe66 <dart.fk712@gmail.com>
2015-10-12 13:26:25 +0600

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 gulp prod
 mvn clean -Dmaven.tomcat.url=http://192.168.200.110:8080/manager/text tomcat7:redeploy 
-scp -r /src/main/resources/public developer@192.168.200.110:/home/sport-equipment-rent
+scp -r src/main/resources/public developer@192.168.200.110:/home/sport-equipment-rent/public
